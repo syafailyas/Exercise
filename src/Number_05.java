8ff530714983c9bb667e4aed96f43c4f0c4ae900
syafailyas <syafailyasalmuzani@gmail.com>
2024-03-20 16:46:56 +0700

    new change

--- src/Number_05.java
+++ src/Number_05.java
@@ -4,19 +4,19 @@ public class Number_05
 {
     public static class NameInitials
     {
-//        public static void main(String[] args)
-//        {
-//            Scanner scanner = new Scanner(System.in);
-//
-//            System.out.print("Enter your full name: ");
-//            String fullName = scanner.nextLine();
-//
-//            String initials = getInitials(fullName);
-//
-//            System.out.println("Your initials in uppercase: " + initials);
-//
-//            scanner.close();
-//        }
+        public static void main(String[] args)
+        {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter your full name: ");
+            String fullName = scanner.nextLine();
+
+            String initials = getInitials(fullName);
+
+            System.out.println("Your initials in uppercase: " + initials);
+
+            scanner.close();
+        }
 
         public static String getInitials(String fullName)
         {
